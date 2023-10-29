264c2c7b81312193636e47ca3ccf4150d9578f20
Nob Yuasys <yuasys@gmail.com>
2023-10-29 09:51:08 +0900

    タプル

--- python-basic/main.py
+++ python-basic/main.py
@@ -1,17 +1,11 @@
-# リスト
-persons = ["tarro", "jiro", "saburo", "shiro"]
-
-# print(persons)
+# タプル
+# persons = ("tarro", "jiro", "saburo")
+# persons[1] = 'hanako'
 # print(persons[1])
 
-# for v in persons:
-#   print(v)
-  
-# persons[2] = "hanako"
-# persons.append("goro")
-
-# print(persons)
-# print(len(persons))
-# print(persons[1:])
-# print(persons[:2])
-print(persons[1:3])
+# タプルの要素が１つの場合は要素の後に「,」が必要
+# colors = ('red') #カンマ,がないとcloreは変数と認識される
+# colors = 'blue'
+# print(colors)
+colors = ('red',)  #カンマがあると正しくタプルと認識される
+# colors[0] = 'blue' #タプルの要素への代入はエラー発生
