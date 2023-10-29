c77fd4018d37671717f76b7a4e7aa09b955a0311
Nob Yuasys <yuasys@gmail.com>
2023-10-29 11:25:09 +0900

    辞書

--- python-basic/main.py
+++ python-basic/main.py
@@ -1,11 +1,15 @@
-# タプル
-# persons = ("tarro", "jiro", "saburo")
-# persons[1] = 'hanako'
-# print(persons[1])
+# 辞書
 
-# タプルの要素が１つの場合は要素の後に「,」が必要
-# colors = ('red') #カンマ,がないとcloreは変数と認識される
-# colors = 'blue'
-# print(colors)
-colors = ('red',)  #カンマがあると正しくタプルと認識される
-# colors[0] = 'blue' #タプルの要素への代入はエラー発生
+person = {'name': 'taro', 'age': 20}
+# print(person)
+# keyが'name'の要素のvalueを表示する
+#print(person['name'])
+
+# for k in person.keys():
+#   print(k)
+  
+# for v in person.values():
+#   print(v)
+for k, v in person.items():
+  print(k, v)
+  
